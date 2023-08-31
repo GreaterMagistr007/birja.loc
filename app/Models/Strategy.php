1218d9f6e565e983e0e3ffefcf36e75cc9d68a56
Aktuganov Konstantin <aktuganovkonstantin@gmail.com>
2023-09-01 02:58:47 +0800

    придумываю Readme

---
+++ app/Models/Strategy.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Strategy extends Model
+{
+    /**
+     * Модель "Стратегия" управляет режимом работы стратегий.
+     */
+    use HasFactory;
+}
